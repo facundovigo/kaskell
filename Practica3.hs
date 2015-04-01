77a30b1bcf857d898e48b42770dbb813a21da3e1
Maricruz <gil.maricruz@gmail.com>
2015-04-01 16:09:26 -0300

    Update Practica3.hs

--- Practica3.hs
+++ Practica3.hs
@@ -19,12 +19,10 @@ sumarT :: Tree Int -> Int
 sumarT EmptyT = 0
 sumarT (NodeT x t1 t2) = x + sumarT t1 + sumarT t2
 
-
 -- 2. Dado un arbol binario devuelve su cantidad de elementos, es decir, el tamano del arbol (size en ingles).
 sizeT :: Tree a -> Int
 sizeT EmptyT = 0
 sizeT (NodeT x t1 t2) = 1 + sizeT t1 + sizeT t2
-
 
 -- 3 Dado un arbol de enteros devuelve un arbol con el doble de cada numero.
 mapDobleT :: Tree Int -> Tree Int
@@ -37,31 +35,48 @@ mapOpuestoT :: Tree Dir -> Tree Dir
 mapOpuestoT EmptyT = EmptyT
 mapOpuestoT (NodeT x t1 t2) = NodeT (opuesto x) (mapOpuestoT t1) (mapOpuestoT t2)
 
-{-
 -- 5 Dado un arbol de palabras devuelve un arbol con la longitud de cada palabra.
 mapLongitudT :: Tree String -> Tree Int
+mapLongitudT EmptyT = EmptyT
+mapLongitudT (NodeT x t1 t2) = NodeT (length x) (mapLongitudT t1) (mapLongitudT t2)
 
 -- 6 Dados un elemento y un arbol binario devuelve True si existe un elemento igual a ese en el arbol.
 perteneceT :: Eq a => a -> Tree a -> Bool
+perteneceT n EmptyT = False
+perteneceT n (NodeT x t1 t2) = ( n == x ) || perteneceT n t1 || perteneceT n t2
 
 -- 7 Dados un elemento e y un arbol binario devuelve la cantidad de elementos del arbol que son iguales a e.
 aparicionesT :: Eq a => a -> Tree a -> Int
+aparicionesT e EmptyT = 0
+aparicionesT e (NodeT x t1 t2) = if e == x
+                                 then 1 + aparicionesT e t1 + aparicionesT e t2
+                                 else aparicionesT e t1 + aparicionesT e t2
 
 -- 8 Dado un arbol de personas devuelve el promedio entre las edades de todas las personas.
 --Definir las subtareas que sean necesarias para resolver esta funcion.
 --Nota: Utilizar el tipo Persona ya definido.
-promedioEdadesT :: Tree Persona -> Int
+--promedioEdadesT :: Tree Persona -> Int
 
 -- 9 Dados dos arboles construye un arbol t en el que ambos arboles son hijos de t, y en la raız de t se guarda la suma de todos los elementos de los hijos de t. ¿Se utiliza recursion para definir esta funcion?
 engancharYSumaEnRaiz :: Tree Int -> Tree Int -> Tree Int
+engancharYSumaEnRaiz EmptyT EmptyT = EmptyT
+engancharYSumaEnRaiz t1 t2 = NodeT (sumarT (NodeT 0 t1 t2)) t1 t2 -- NO HACE FALTA RECURSIVIDAD
 
+{-
 -- 10 Dado un arbol devuelve su cantidad de hojas.
 --Nota: una hoja (leaf en ingles) es un nodo que no tiene hijos.
 leaves :: Tree a -> Int
+leaves EmptyT = 0
+leaves NodeT x t1 t2 = if (t1 == EmptyT && t2 == EmptyT)
+                       then 1 + leaves t1 + leaves t2
+                       else leaves t1 + leaves t2
 
 -- 11 Dado un arbol devuelve su altura.
 --Nota: la altura (height en ingles) de un arbol es la cantidad maxima de nodos entre la raız y alguna de sus hojas. La altura de un arbol vacıo es cero y la de una hoja es 1.
 heightT :: Tree a -> Int
+heightT EmptyT = 0
+heightT (NodeT x t1 t2) = if (t1 == EmptyT && t2 == EmptyT)
+                          then 
 
 -- 12 Dado un arbol devuelve el numero de nodos que no son hojas. ¿Como podrıa resolverla sin utilizar recursion? Primero defınala con recursion y despues sin ella.
 nodes :: Tree a -> Int
@@ -83,7 +98,6 @@ listPosOrder :: Tree a -> [a]
 
 -- 17 Dado un arbol de listas devuelve la concatenacion de todas esas listas. El recorrido debe ser in-order.
 concatenarListasT :: Tree [a] -> [a]
-
 
 -- 18 Dados un numero n y un arbol devuelve una lista con los nodos de nivel n.
 --Nota: El primer nivel de un arbol (su ra ́ız) es 0.
