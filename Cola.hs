55d456b0d267089a7b7837c1e6b81c07b9205c78
Maricruz <gil.maricruz@gmail.com>
2015-04-13 17:19:01 -0300

    Update Cola.hs

--- Cola.hs
+++ Cola.hs
@@ -1,8 +1,16 @@
 
+-- Practica 4
+-- 1.2
 -- Rol Implementador
+
+module Colas (Queue, emptyQ, queueQ, firstQ, isEmptyQ, deQueueQ)
+where
 
 data Queue a = Consq [a] deriving Show
 
+-- funcion de acceso
+dameLista :: Queue a -> [a]
+dameLista (Consq [a]) = [a]
 
 -- retorna una cola vacia
 emptyQ :: Queue a
@@ -10,16 +18,16 @@ emptyQ = Consq []
 
 -- agrega un elemento a la cola
 queueQ :: Queue a -> a -> Queue a
-queueQ (Consq [a]) n = (Consq (n : [a]))
+queueQ q n = (Consq (n : dameLista q))
 
 -- retorna el primer elemento de la cola
 firstQ :: Queue a -> a 
-firstQ (Consq [a]) = last [a]
+firstQ q = last (dameLista q)
 
 -- pregunta si la cola esta vacia
 isEmptyQ :: Queue a -> Bool
-isEmptyQ (Consq [a]) = null [a]
+isEmptyQ q = null (dameLista q)
 
 -- quita el primero elemento de la cola y devuelve el resto
 deQueueQ :: Queue a -> Queue a
-deQueueQ (Consq [a]) = Consq (init [a])
+deQueueQ q = Consq (init (dameLista q))
